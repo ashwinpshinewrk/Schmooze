18884352fc08ace95f566e43f13e2210ee3179a2
prisoneroflove <ashwinshine.nonu@gmail.com>
2024-07-15 15:52:21 +0530

    changes to values, added assets

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
-@export var MAX_SPEED = 300
+@export var MAX_SPEED = 150
 @export var ACCELARTION = 1500
-@export var FRICTION = 1200
+@export var FRICTION = 20000 - 500
 
 @onready var axis = Vector2.ZERO
 
